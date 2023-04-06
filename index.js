8c7a94deef40d962f964b5ece7e1327bf94cc058
iceblackhot <iceblackhot2@gmail.com>
2023-04-06 20:28:45 +0300

    try fix CORS

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', process.env.FRONT_HOST);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 const routes = require('./settings/routes');
 routes(app);
 
 app.listen(process.env.HOST_PORT, () => {
-  console.log(`App listen on port ${process.env.HOST_PORT | process.env.HOST}`);
+  console.log(`App listen on port ${process.env.HOST_PORT | process.env.HOST_SERVER}`);
 });
