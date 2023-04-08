bb835baa9a1d598bacb9e2a0143664cfc38f81dc
iceblackhot <iceblackhot2@gmail.com>
2023-04-08 11:08:32 +0300

    try fix CORS 2

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ require('dotenv').config();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(cors());
-
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', process.env.FRONT_HOST);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(
+  cors({
+    origin: process.env.FRONT_HOST,
+  }),
+);
 
 const routes = require('./settings/routes');
 routes(app);
